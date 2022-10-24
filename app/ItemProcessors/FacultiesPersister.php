7d3837a8b60edc81532c6e591bdcf3cb40ece4fe
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-10-24 09:47:58 +0200

    Created `FacultiesPersister`

---
+++ app/ItemProcessors/FacultiesPersister.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\ItemProcessors;
+
+use App\Models\Faculty;
+use RoachPHP\ItemPipeline\ItemInterface;
+use RoachPHP\ItemPipeline\Processors\ItemProcessorInterface;
+use RoachPHP\Support\Configurable;
+
+class FacultiesPersister implements ItemProcessorInterface
+{
+    use Configurable;
+
+    public function processItem(ItemInterface $item): ItemInterface
+    {
+        $faculties = $item->all();
+        $findBy = ['name', 'link'];
+        $columnsToUpdate = ['name', 'link'];
+
+        Faculty::upsert(
+            $faculties, 
+            $findBy, 
+            $columnsToUpdate,
+        );
+        
+        return $item;
+    }
+}
