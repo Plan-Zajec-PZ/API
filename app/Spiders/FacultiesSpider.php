6d290c9a5937446b3c3aef2bd39aa4964b6a6f38
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-10-24 09:48:05 +0200

    Created `FacultiesSpider`

---
+++ app/Spiders/FacultiesSpider.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Spiders;
+
+use App\ItemProcessors\FacultiesPersister;
+use Generator;
+use RoachPHP\Downloader\Middleware\RequestDeduplicationMiddleware;
+use RoachPHP\Extensions\LoggerExtension;
+use RoachPHP\Extensions\StatsCollectorExtension;
+use RoachPHP\Http\Response;
+use RoachPHP\Spider\BasicSpider;
+use RoachPHP\Spider\ParseResult;
+use Symfony\Component\DomCrawler\Crawler;
+
+class FacultiesSpider extends BasicSpider
+{
+    public array $startUrls = [
+        'http://www.plan.pwsz.legnica.edu.pl',
+    ];
+
+    public array $downloaderMiddleware = [
+        RequestDeduplicationMiddleware::class,
+    ];
+
+    public array $spiderMiddleware = [
+        //
+    ];
+
+    public array $itemProcessors = [
+        FacultiesPersister::class,
+    ];
+
+    public array $extensions = [
+        LoggerExtension::class,
+        StatsCollectorExtension::class,
+    ];
+
+    public int $concurrency = 2;
+
+    public int $requestDelay = 1;
+
+    /**
+     * @return Generator<ParseResult>
+     */
+    public function parse(Response $response): Generator
+    {
+        $faculties = $response->filterXPath('//div[contains(@class, "page-sidebar")]//li/a');
+
+        $results = $faculties->each(fn (Crawler $node) => [
+            'name' => $node->text(),
+            'link' => $node->link()->getUri(),
+        ]);
+
+        yield $this->item($results);
+    }
+}
