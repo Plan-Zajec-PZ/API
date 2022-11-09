602cfd0e020a5359935f0b57dc568bd14ad23034
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-11-09 16:49:23 +0100

    Added `lecturers` table

---
+++ database/migrations/2022_11_09_114519_create_lecturers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lecturers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('faculty_id')->constrained()->cascadeOnDelete();
+            $table->string('name', 255)->unique();
+            $table->string('link', 255)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lecturers');
+    }
+};
