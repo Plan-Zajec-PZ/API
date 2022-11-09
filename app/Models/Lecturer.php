508b2ec9370f53d0fcab9507016d86e357cdb2c8
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-11-09 16:50:14 +0100

    Added `Lecturer` model with relationship

--- app/Models/Lecturer.php
+++ app/Models/Lecturer.php
@@ -4,9 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Faculty extends Model
+class Lecturer extends Model
 {
     use HasFactory;
 
@@ -15,8 +15,8 @@ class Faculty extends Model
         'link',
     ];
 
-    public function lecturers(): HasMany
+    public function faculty(): BelongsTo
     {
-        return $this->hasMany(Lecturer::class);
+        return $this->belongsTo(Faculty::class);
     }
 }
