f4ae4a84a29e6ace231585234753b51b24fd8346
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-11-12 15:14:15 +0100

    Change `link` column in `lecturers` table

--- database/migrations/2022_11_09_114519_create_lecturers_table.php
+++ database/migrations/2022_11_09_114519_create_lecturers_table.php
@@ -16,7 +16,7 @@ return new class () extends Migration {
             $table->id();
             $table->foreignId('faculty_id')->constrained()->cascadeOnDelete();
             $table->string('name', 255);
-            $table->string('link', 255);
+            $table->string('link', 255)->unique();
             $table->timestamps();
         });
     }
