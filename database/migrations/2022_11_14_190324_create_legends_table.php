61c4aa7d8795e9febb648f8475ee7d382ed607d6
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-11-15 17:24:46 +0100

    Added `legends` table

---
+++ database/migrations/2022_11_14_190324_create_legends_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('legends', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('schedule_id')->constrained()->cascadeOnDelete();
+            $table->string('abbreviation', 50);
+            $table->string('fullname', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('legends');
+    }
+};
