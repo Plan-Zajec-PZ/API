b2ca1537dc13ca9b841720e892c7bec12a7e32b1
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-11-15 17:26:10 +0100

    Added relations in `Lecturer` model

--- app/Models/Lecturer.php
+++ app/Models/Lecturer.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Lecturer extends Model
 {
@@ -18,5 +19,17 @@ class Lecturer extends Model
     public function faculty(): BelongsTo
     {
         return $this->belongsTo(Faculty::class);
+    }
+
+    public function schedule(): HasOne
+    {
+        return $this->hasOne(Schedule::class);
+    }
+
+    protected static function booted()
+    {
+        static::created(function ($lecturer) {
+            $lecturer->schedule()->create();
+        });
     }
 }
