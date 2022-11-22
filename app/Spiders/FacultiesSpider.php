07fe248b7302322aa5d82253ec9b7d553c55acc6
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-11-22 17:33:08 +0100

    Added `ResponseEncodingCorrection`

--- app/Spiders/FacultiesSpider.php
+++ app/Spiders/FacultiesSpider.php
@@ -3,6 +3,7 @@
 namespace App\Spiders;
 
 use App\ItemProcessors\FacultiesPersister;
+use App\SpiderMiddlewares\ResponseEncodingCorrection;
 use Generator;
 use RoachPHP\Downloader\Middleware\RequestDeduplicationMiddleware;
 use RoachPHP\Extensions\LoggerExtension;
@@ -19,6 +20,7 @@ class FacultiesSpider extends BasicSpider
 
     public array $downloaderMiddleware = [
         RequestDeduplicationMiddleware::class,
+        ResponseEncodingCorrection::class,
     ];
 
     public array $itemProcessors = [
