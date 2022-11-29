1ea3af31bef950a8c5cc97136c38817355528727
Anna-Sokolowska <anna.sokolowska343@gmail.com>
2022-11-29 02:50:25 +0100

    Create GroupSchedules Spider

---
+++ app/ItemProcessors/MajorsSchedulesPersister.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\ItemProcessors;
+
+use App\Models\Group;
+use App\Models\Specialization;
+use RoachPHP\ItemPipeline\ItemInterface;
+use RoachPHP\ItemPipeline\Processors\ItemProcessorInterface;
+use RoachPHP\Support\Configurable;
+
+class MajorsSchedulesPersister implements ItemProcessorInterface
+{
+    use Configurable;
+
+    public function processItem(ItemInterface $item): ItemInterface
+    {
+        $groups = $item->all()['groups'];
+        $specialization = Specialization::firstWhere('link', $item->all()['specialization_page_link'],);
+        $majorScheduleItems = $item->all()['dailySchedules'];
+
+        foreach ($majorScheduleItems as &$majorScheduleItem) {
+            $majorScheduleItem['schedule'] = $this->getGroupDailySchedules($groups, $majorScheduleItem['schedule']);
+        }
+
+        $majorScheduleItems = $this->getGroupSchedules($groups, $majorScheduleItems);
+
+        $this->persistGroups($item->all()['groups'], $specialization);
+        $this->persistGroupSchedules($majorScheduleItems);
+        return $item;
+    }
+
+    public function getGroupDailySchedules($groups, $dailySchedule): array
+    {
+        $new = [];
+        foreach ($groups as $group) {
+            $new[$group] = [];
+            $hours = [];
+            foreach ($dailySchedule as $hour => $value) {
+                $hours[$hour] = $value[$group];
+            }
+            $new[$group] = $hours;
+        }
+
+        return $new;
+    }
+
+    public function getGroupSchedules($groups, $schedule): array
+    {
+        $new = [];
+        foreach ($groups as $group) {
+            $new[$group] = [];
+            $days = [];
+            foreach ($schedule as $day) {
+                $days[$day['day']] = $day['schedule'][$group];
+            }
+            $new[$group] = $days;
+        }
+
+        return $new;
+    }
+
+    public function persistGroups($itemGroups, $specialization)
+    {
+        foreach ($itemGroups as $itemGroup) {
+            $group = Group::firstOrNew([
+                'name' => $itemGroup,
+            ]);
+
+            $group->specialization()->associate($specialization);
+            $group->groupSchedule()->firstOrCreate();
+
+            $group->save();
+        }
+    }
+
+    public function persistGroupSchedules($majorScheduleItems)
+    {
+        foreach ($majorScheduleItems as $group => $majorScheduleItem) {
+            $group = Group::firstWhere([
+                'name' => $group,
+            ]);
+
+            $groupSchedule = $group->groupSchedule();
+
+            $groupSchedule->update([
+                'content' => json_encode($majorScheduleItem)
+            ]);
+        }
+    }
+}
---
+++ app/Spiders/MajorSchedulesSpider.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Spiders;
+
+use App\ItemProcessors\MajorsSchedulesPersister;
+use App\Models\Specialization;
+use Generator;
+use RoachPHP\Downloader\Middleware\RequestDeduplicationMiddleware;
+use RoachPHP\Extensions\LoggerExtension;
+use RoachPHP\Extensions\StatsCollectorExtension;
+use RoachPHP\Http\Request;
+use RoachPHP\Http\Response;
+use RoachPHP\Spider\BasicSpider;
+use RoachPHP\Spider\ParseResult;
+use Symfony\Component\DomCrawler\Crawler;
+
+class MajorSchedulesSpider extends BasicSpider
+{
+    public array $downloaderMiddleware = [
+        RequestDeduplicationMiddleware::class,
+    ];
+
+    public array $itemProcessors = [
+        MajorsSchedulesPersister::class
+    ];
+
+    public array $extensions = [
+        LoggerExtension::class,
+        StatsCollectorExtension::class,
+    ];
+
+    public int $concurrency = 2;
+
+    public int $requestDelay = 1;
+
+    /** @return Request[] */
+    protected function initialRequests(): array
+    {
+        $requests = Specialization::all()
+            ->map(function ($faculty) {
+                return new Request(
+                    'GET',
+                    $faculty->link,
+                    [$this, 'parse']
+                );
+            });
+
+        return $requests->toArray();
+    }
+
+    /**
+     * @return Generator<ParseResult>
+     */
+    public function parse(Response $response): Generator
+    {
+        $scheduleTable = $this->getScheduleTable($response);
+
+        $groups = $this->getGroupsFromSchedule($scheduleTable);
+        $days = $scheduleTable->filter('tr > td.nazwaDnia');
+
+        $dailySchedules = $days->each(fn(Crawler $day) => [
+            'day' => $day->text(),
+            'schedule' => $this->getDailySchedule($day, $groups),
+        ]);
+
+        $result['specialization_page_link'] = $response->getUri();
+        $result['groups'] = $groups;
+        $result['dailySchedules'] = $dailySchedules;
+
+
+        yield $this->item($result);
+    }
+
+    private function getScheduleTable(Response $response): Crawler
+    {
+        return $response
+            ->filter('table.TabPlan:first-of-type ')
+            ->first();
+    }
+
+    private function getGroupsFromSchedule(Crawler $scheduleTable): array
+    {
+        return $scheduleTable
+            ->filter('tr:first-of-type > td.nazwaSpecjalnosci')
+            ->each(
+                fn(Crawler $node) => $node->text()
+            );
+    }
+
+    private function getDailySchedule(Crawler $day, $groups): array
+    {
+        $hoursNode = $day->closest('tr')->siblings()->children('td.godzina');
+        $hours = $hoursNode->each(fn(Crawler $node) => $node->text());
+        $subjects = $hoursNode->each(fn(Crawler $hour) => $hour->nextAll()->each(fn(Crawler $tr) => $tr->text()));
+
+        foreach ($subjects as &$value) {
+            $value = array_chunk($value, 3);
+            $value = array_combine($groups, $value);
+        }
+
+        return array_combine($hours, $subjects);
+    }
+}
