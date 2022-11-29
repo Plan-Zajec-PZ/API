efce8de132c1db746e86c5db54f626377a5eb7e2
Anna-Sokolowska <anna.sokolowska343@gmail.com>
2022-11-29 01:57:55 +0100

    Make Group Model

---
+++ app/Models/Group.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Group extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function specialization(): BelongsTo
+    {
+        return $this->belongsTo(Specialization::class);
+    }
+
+    public function groupSchedule(): HasOne
+    {
+        return $this->HasOne(GroupSchedule::class);
+    }
+}
