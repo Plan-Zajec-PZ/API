dd43d64c44948f763b9deefb3b8b678d27c0278c
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-12-08 18:32:48 +0100

    Relocated logic from `LecturerController` to `RetrieveLecturersAction`

--- app/Http/Controllers/LecturerController.php
+++ app/Http/Controllers/LecturerController.php
@@ -2,31 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\RetrieveLecturersAction;
 use App\Http\Requests\IndexLecturersRequest;
 use App\Http\Resources\LecturerResource;
 use App\Models\Lecturer;
-use Illuminate\Support\Facades\Cache;
 
 class LecturerController extends Controller
 {
-    public function index(IndexLecturersRequest $request): mixed
+    public function index(IndexLecturersRequest $request, RetrieveLecturersAction $action): mixed
     {
         $facultyId = $request->validated('faculty');
 
-        $key = 'lecturers-' . $facultyId ?? 'all';
-        $seconds = 60 * 60;
-        $callback = $this->buildCallback($facultyId);
-
-        return Cache::remember($key, $seconds, $callback);
-    }
-
-    private function buildCallback(?int $facultyId): \Closure
-    {
-        $whenCallback = fn ($query) => $query->where('faculty_id', $facultyId);
-
-        return fn () => Lecturer::query()
-            ->when($facultyId, $whenCallback)
-            ->pluck('name', 'id');
+        return $action->execute($facultyId);
     }
 
     public function show(Lecturer $lecturer): LecturerResource
