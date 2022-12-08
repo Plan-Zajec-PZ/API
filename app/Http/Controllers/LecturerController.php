404d4a077fdc13a5798bcbcd8cc195668d867754
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-12-08 17:10:16 +0100

    Added logic to `LecturerController`

--- app/Http/Controllers/LecturerController.php
+++ app/Http/Controllers/LecturerController.php
@@ -2,17 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\IndexLecturersRequest;
+use App\Http\Resources\LecturerResource;
 use App\Models\Lecturer;
+use Illuminate\Support\Facades\Cache;
 
 class LecturerController extends Controller
 {
-    public function index()
+    public function index(IndexLecturersRequest $request): mixed
     {
-        //
+        $facultyId = $request->validated('faculty');
+
+        $key = 'lecturers-' . $facultyId ?? 'all';
+        $seconds = 60 * 60;
+        $callback = $this->buildCallback($facultyId);
+
+        return Cache::remember($key, $seconds, $callback);
     }
 
-    public function show(Lecturer $lecturer)
+    private function buildCallback(?int $facultyId): \Closure
     {
-        //
+        $whenCallback = fn($query) => $query->where('faculty_id', $facultyId);
+
+        return fn() => Lecturer::query()
+            ->when($facultyId, $whenCallback)
+            ->pluck('name', 'id');
+    }
+
+    public function show(Lecturer $lecturer): LecturerResource
+    {
+        return new LecturerResource($lecturer);
     }
 }
