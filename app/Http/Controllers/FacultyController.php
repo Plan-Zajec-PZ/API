665fa5043ef3bdf7c01bc46ec209b0260fbafdb7
Anna-Sokolowska <anna.sokolowska343@gmail.com>
2023-01-03 02:49:57 +0100

    Add FacultyController index method

--- app/Http/Controllers/FacultyController.php
+++ app/Http/Controllers/FacultyController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\FacultyResource;
 use App\Models\Faculty;
 
 class FacultyController extends Controller
 {
     public function index()
     {
-        //
+        return FacultyResource::collection(Faculty::all());
     }
 }
