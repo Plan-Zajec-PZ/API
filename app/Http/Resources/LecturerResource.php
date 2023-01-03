293217674e80e39a8dca76789817cd61c92a7c68
Grzegorz Bogusz <gbogusz55@gmail.com>
2023-01-03 14:28:46 +0100

    Removed redundant complexity from `LecturerResource`

--- app/Http/Resources/LecturerResource.php
+++ app/Http/Resources/LecturerResource.php
@@ -2,37 +2,16 @@
 
 namespace App\Http\Resources;
 
-use App\Models\Schedule;
-use App\Spiders\LecturerScheduleSpider;
 use Illuminate\Http\Resources\Json\JsonResource;
-use RoachPHP\Roach;
 
 class LecturerResource extends JsonResource
 {
     public function toArray($request): array
     {
-        $schedule = $this->schedule ?? $this->scrapSchedule();
-
         return [
             'name' => $this->name,
             'faculty' => new FacultyResource($this->faculty),
-            'schedule' => new ScheduleResource($schedule),
+            'schedule' => new ScheduleResource($this->schedule),
         ];
-    }
-
-    private function scrapSchedule(): Schedule
-    {
-        $context = [
-            'models' => [$this->resource]
-        ];
-
-        Roach::collectSpider(
-            LecturerScheduleSpider::class,
-            context: $context
-        );
-
-        $this->resource->refresh();
-
-        return $this->schedule;
     }
 }
