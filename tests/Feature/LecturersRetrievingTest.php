ff52b2bd335dc15dd9311f2883c6fadb75785818
Grzegorz Bogusz <gbogusz55@gmail.com>
2023-01-03 13:40:19 +0100

    Added `LecturersRetrievingTest`

---
+++ tests/Feature/LecturersRetrievingTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Faculty;
+use App\Models\Lecturer;
+use Database\Seeders\LecturersSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class LecturersRetrievingTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed(LecturersSeeder::class);
+    }
+
+    public function testAllLecturersCanBeRetrieved()
+    {
+        $route = route('lecturers.index');
+
+        $response = $this->getJson($route);
+        $response->assertOk();
+
+        $expectedNumber = Lecturer::query()->count();
+        $response->assertJsonCount($expectedNumber);
+    }
+
+    public function testLecturersFromSpecificFaultyCanBeRetrieved()
+    {
+        $selectedFaculty = Faculty::all()->random();
+
+        $route = route('lecturers.index', [
+            'faculty' => $selectedFaculty->id,
+        ]);
+
+        $response = $this->getJson($route);
+        $response->assertOk();
+
+        $expectedNumber = $selectedFaculty->lecturers()->count();
+        $response->assertJsonCount($expectedNumber);
+    }
+
+    public function testRequestWithInvalidFacultyIdIsRejected()
+    {
+        Faculty::query()->delete();
+
+        $route = route('lecturers.index', [
+            'faculty' => random_int(1, 100)
+        ]);
+
+        $response = $this->getJson($route);
+        $response->assertUnprocessable();
+    }
+}
