e6f93be7ab2c6bcb7bb52a22e0d01216461b8a2f
Anna-Sokolowska <anna.sokolowska343@gmail.com>
2023-01-03 19:16:54 +0100

    Add data to Faculty Resource

--- app/Http/Resources/FacultyResource.php
+++ app/Http/Resources/FacultyResource.php
@@ -9,7 +9,10 @@ class FacultyResource extends JsonResource
     public function toArray($request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
+            'majors' => MajorResource::collection($this->whenLoaded('majors')),
+            'lecturers' => LecturerResource::collection($this->whenLoaded('lecturers')),
         ];
     }
 }
