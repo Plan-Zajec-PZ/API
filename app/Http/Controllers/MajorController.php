856f066d088456548488c01332a2113714204947
Anna-Sokolowska <anna.sokolowska343@gmail.com>
2023-01-03 20:26:34 +0100

    Update Major Controller

--- app/Http/Controllers/MajorController.php
+++ app/Http/Controllers/MajorController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\MajorResource;
+use App\Models\Faculty;
 use App\Models\Major;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class MajorController extends Controller
 {
-    public function index()
+    public function index(Faculty $faculty): ResourceCollection
     {
-        //
+        return MajorResource::collection(Major::query()->whereBelongsTo($faculty)->with('specializations')->get());
+    }
+
+    public function show(Faculty $faculty, Major $major): MajorResource
+    {
+        return new MajorResource($major->load('specializations'));
     }
 }
