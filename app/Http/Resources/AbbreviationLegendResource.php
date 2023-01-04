312fa6c80a17cbefee6453147ba2f55a1aaf88b6
Anna-Sokolowska <Anna-Sokolowska@users.noreply.github.com>
2023-01-04 05:06:45 +0000

    Automated PHP Linting (Pint)

--- app/Http/Resources/AbbreviationLegendResource.php
+++ app/Http/Resources/AbbreviationLegendResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class  AbbreviationLegendResource extends JsonResource
+class AbbreviationLegendResource extends JsonResource
 {
     public function toArray($request): array
     {
