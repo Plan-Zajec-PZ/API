8d7edac816c9e9d3de406339906de2e9d71801d3
Grzegorz Bogusz <gbogusz55@gmail.com>
2023-01-06 09:56:21 +0100

    Added `RunLecturerScheduleSpider` command

---
+++ app/Console/Commands/RunLecturerScheduleSpider.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Lecturer;
+use App\Spiders\LecturerScheduleSpider;
+use Illuminate\Console\Command;
+use RoachPHP\Roach;
+
+class RunLecturerScheduleSpider extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'scrap:lecturer {lecturer}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run spider to scrap specific lecturer with related schedule';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        try {
+            $lecturer = $this->findLecturer();
+
+            Roach::startSpider(
+                LecturerScheduleSpider::class,
+                context: $this->buildContext($lecturer)
+            );
+        } catch (\Throwable) {
+            return Command::FAILURE;
+        }
+
+        return Command::SUCCESS;
+    }
+
+    private function findLecturer(): Lecturer
+    {
+        $lecturerId = $this->argument('lecturer');
+
+        return Lecturer::query()->findOrFail($lecturerId);
+    }
+
+    private function buildContext(Lecturer $lecturer): array
+    {
+        return [
+            'models' => [$lecturer],
+        ];
+    }
+}
