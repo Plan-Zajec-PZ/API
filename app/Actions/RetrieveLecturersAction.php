c5311f89e8b85104fa9ce82bdfaff14248dc57b0
Grzegorz Bogusz <gbogusz55@gmail.com>
2023-01-06 08:49:29 +0100

    Removed single cache usage

--- app/Actions/RetrieveLecturersAction.php
+++ app/Actions/RetrieveLecturersAction.php
@@ -3,46 +3,16 @@
 namespace App\Actions;
 
 use App\Models\Lecturer;
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Collection;
 
 class RetrieveLecturersAction
 {
-    protected ?int $facultyId;
-
-    public function execute(?int $facultyId = null): mixed
+    public function execute(?int $facultyId): Collection
     {
-        $this->facultyId = $facultyId;
+        $callback = fn ($query) => $query->where('faculty_id', $facultyId);
 
-        return Cache::remember(
-            $this->buildKey(),
-            $this->calculateExpirationTime(),
-            $this->buildCallback()
-        );
-    }
-
-    private function buildKey(): string
-    {
-        $entity = 'lecturers';
-        $separator = '-';
-        $selected = $this->facultyId ?? 'all';
-
-        return $entity . $separator . $selected;
-    }
-
-    private function calculateExpirationTime(): Carbon
-    {
-        return now()->addHour();
-    }
-
-    private function buildCallback(): \Closure
-    {
-        $callback = function ($query) {
-            return $query->where('faculty_id', $this->facultyId);
-        };
-
-        return fn () => Lecturer::query()
-            ->when($this->facultyId, $callback)
+        return Lecturer::query()
+            ->when($facultyId, $callback)
             ->pluck('name', 'id');
     }
 }
