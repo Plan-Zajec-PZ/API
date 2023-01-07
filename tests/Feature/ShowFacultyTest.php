844b54adedbefa38e0939a88aae56520c1d4cbee
Grzegorz Bogusz <gbogusz55@gmail.com>
2023-01-07 10:36:34 +0100

    Added `ShowFacultyTest`

---
+++ tests/Feature/ShowFacultyTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Faculty;
+use Database\Seeders\FacultiesSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ShowFacultyTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed(FacultiesSeeder::class);
+
+        $this->withHeader(
+            'Authorization',
+            'Bearer ' . config('security.remote_key')
+        );
+    }
+
+    public function testSpecificFacultyCanBeRetrieved()
+    {
+        $faculty = Faculty::all()->random();
+
+        $route = route('faculties.show', ['faculty' => $faculty->id]);
+
+        $response = $this->getJson($route);
+        $response->assertOk();
+
+        $response->assertJsonStructure([
+            'data' => [
+                'id',
+                'name',
+                'majors'
+            ]
+        ]);
+    }
+
+    public function testRequestWithInvalidFacultyIdIsRejected()
+    {
+        Faculty::query()->delete();
+        $this->assertDatabaseEmpty('faculties');
+
+        $route = route('faculties.show', [
+            'faculty' => random_int(1, 100)
+        ]);
+
+        $response = $this->getJson($route);
+        $response->assertNotFound();
+    }
+}
