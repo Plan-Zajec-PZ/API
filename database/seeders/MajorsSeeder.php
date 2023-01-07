3f75bf3309e5c539b7e3fc6027dda86c886275f8
Grzegorz Bogusz <gbogusz55@gmail.com>
2023-01-07 11:30:13 +0100

    Added relation to `MajorsSeeder`

--- database/seeders/MajorsSeeder.php
+++ database/seeders/MajorsSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Faculty;
 use App\Models\Major;
+use App\Models\Specialization;
 use Illuminate\Database\Seeder;
 
 class MajorsSeeder extends Seeder
@@ -15,9 +16,12 @@ class MajorsSeeder extends Seeder
      */
     public function run()
     {
+        $specialization = Specialization::factory();
+
         Major::factory()
             ->count(5)
             ->for(Faculty::factory())
+            ->has($specialization)
             ->create();
     }
 }
