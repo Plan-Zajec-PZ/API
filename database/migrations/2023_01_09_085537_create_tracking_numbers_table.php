5b0a2f08d05bd2ca8a57a952db51763a696db8ee
Grzegorz Bogusz <gbogusz55@gmail.com>
2023-01-09 15:36:18 +0100

    Changed `hash` column to unique in `tracking_numbers` table

--- database/migrations/2023_01_09_085537_create_tracking_numbers_table.php
+++ database/migrations/2023_01_09_085537_create_tracking_numbers_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('tracking_numbers', function (Blueprint $table) {
             $table->id();
-            $table->string('hash', 255);
+            $table->string('hash', 255)->unique();
             $table->timestamps();
         });
     }
