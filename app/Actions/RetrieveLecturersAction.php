4732ffb52a1d20007c6dc40a939ff48b92834f5c
Grzegorz Bogusz <gbogusz55@gmail.com>
2023-01-11 05:42:05 +0100

    Added `RetrieveLecturersAction`

--- app/Actions/RetrieveLecturersAction.php
+++ app/Actions/RetrieveLecturersAction.php
@@ -3,16 +3,46 @@
 namespace App\Actions;
 
 use App\Models\Lecturer;
-use Illuminate\Support\Collection;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Cache;
 
 class RetrieveLecturersAction
 {
-    public function execute(?int $facultyId): Collection
-    {
-        $callback = fn ($query) => $query->where('faculty_id', $facultyId);
+    protected ?int $facultyId;
 
-        return Lecturer::query()
-            ->when($facultyId, $callback)
-            ->get(['id', 'name']);
+    public function execute(?int $facultyId = null): mixed
+    {
+        $this->facultyId = $facultyId;
+
+        return Cache::remember(
+            $this->buildKey(),
+            $this->calculateExpirationTime(),
+            $this->buildCallback()
+        );
+    }
+
+    private function buildKey(): string
+    {
+        $entity = 'lecturers';
+        $separator = '-';
+        $selected = $this->facultyId ?? 'all';
+
+        return $entity . $separator . $selected;
+    }
+
+    private function calculateExpirationTime(): Carbon
+    {
+        return now()->addHours(3);
+    }
+
+    private function buildCallback(): \Closure
+    {
+        $callback = function ($query) {
+            return $query->where('faculty_id', $this->facultyId);
+        };
+
+        return fn () => Lecturer::query()
+            ->when($this->facultyId, $callback)
+            ->pluck('name', 'id');
     }
 }
